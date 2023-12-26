24943f68edc11529de24f890228e5b2da31db80c
serhat <confxsd@gmail.com>
2023-12-26 21:34:07 +0300

    v1

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+export interface AppConfig {
+  name: string;
+  website: string;
+  version: string;
+  description: string;
+  apiUrl: string;
+  ipfsGateway: string;
+};
+
+export const appConfig: AppConfig = {
+  name: 'Index Chat',
+  website: 'https://index.network',
+  version: '1',
+  description: 'Chat with your indexes, easy as pie.',
+  apiUrl: 'https://index.network',
+  ipfsGateway: 'https://indexas.infura-ipfs.io/ipfs',
+};
